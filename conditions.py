fb6b84cc2688317131683b8fb7e2a5ba3a3b0e5a
Evans-mutuku <evansnyamai98@gmail.com>
2024-03-13 12:43:43 +0300

    Python Hackathon

---
+++ conditions.py
@@ -0,0 +1,9 @@
+# Python Conditional Statements
+
+
+# Create a Python program that:
+
+
+# - Prompts a user to enter their age.
+# - Uses a conditional statement to check if the age is greater than or equal to 18.
+# - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
