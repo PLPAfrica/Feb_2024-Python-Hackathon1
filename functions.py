b0a7b26efee964b188f8ad3dc76f22b70bd09d9e
Evans-mutuku <evansnyamai98@gmail.com>
2024-03-13 13:16:20 +0300

    Python Hackathon

---
+++ functions.py
@@ -0,0 +1,10 @@
+# Functions & Fibonacci Sequence
+# Write a Python program to generate the Fibonacci sequence up to a specified term n. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms.
+
+
+
+# Your program should:
+
+# Ask the user to input the value of n.
+# Create a function that takes n as a parameter and returns a list containing the first n terms of the Fibonacci sequence.
+# Print the generated Fibonacci sequence.
