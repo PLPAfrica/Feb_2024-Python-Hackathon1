22c953580ce5808a35eaa2f9494af10e47ec37e3
Evans-mutuku <60472825+Evans-mutuku@users.noreply.github.com>
2024-03-14 13:47:49 +0300

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,4 +1,6 @@
-# Python Conditional Statements
+# Python Conditional Statements 
+#example is https://admin.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
+
 
 
 # Create a Python program that:
