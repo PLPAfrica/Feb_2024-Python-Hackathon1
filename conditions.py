f94e797109c6cb849757f9175585bc780e510ba8
teresiahwambuikaberia <94245422+wambui021@users.noreply.github.com>
2024-03-17 02:27:17 +0300

    Add files via upload

--- conditions.py
+++ conditions.py
@@ -1,11 +1,27 @@
 # Python Conditional Statements 
-#example is https://plpacademy.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
-
-
-
-# Create a Python program that:
-
-
-# - Prompts a user to enter their age.
+def main():
+# - Prompts a user to enter their age.        
+        age = int(input("Please enter your age: "))
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
+        if age >= 18:
+            print("You are eligible to vote.")
+        else:
+            print("You are not eligible to vote.")
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+print("Please enter a valid integer age.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
