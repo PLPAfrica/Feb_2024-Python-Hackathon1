84a9ce4ea39a6c3d1cd49b69a5807da57a6a70f6
Mark Munyao <159567933+Markmunyao@users.noreply.github.com>
2024-03-16 02:41:10 -0700

    Update conditions.py
    hackthon submission

--- conditions.py
+++ conditions.py
@@ -1,11 +1,12 @@
-# Python Conditional Statements 
-#example is https://plpacademy.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
-
-
-
-# Create a Python program that:
-
-
-# - Prompts a user to enter their age.
-# - Uses a conditional statement to check if the age is greater than or equal to 18.
-# - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+#ask to enter age
+age= int(input("Enter your age:"))
+#initialize result variable
+result=""
+#check if status
+if age>=18:
+    result="You are eligible to vote"
+elif age<18:
+    result="You are not eligible to vote"
+#print the result
+print(result)    
+    
