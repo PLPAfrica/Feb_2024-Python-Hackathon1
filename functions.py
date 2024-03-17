da865ce35d229e8d391a6891b47890f6410e4dc5
moureebargoge@gmail.com <moureenbargoge@gmail.com>
2024-03-17 20:40:54 +0300

    fibonacci

--- functions.py
+++ functions.py
@@ -1,41 +1,27 @@
-# Functions & Fibonacci Sequence
-# Question
-# Write a Python program to generate the Fibonacci sequence up to a specified term n. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms.
-#We have provided  you with in-complete code, from the Knowledge learned from week 1 to week 3 please complete the missing parts to achieve the goal of the question.
-def fibonacci(n):
-  """
-  This function generates the Fibonacci sequence up to a specified term n using iteration.
+def generate_fibonacci(n):
+    """
+    Generate the Fibonacci sequence up to the nth term.
 
-  Args:
-      n: The number of terms in the Fibonacci sequence.
+    Parameters:
+        n (int): The number of terms in the Fibonacci sequence to generate.
 
-  Returns:
-      A list containing the Fibonacci sequence up to n terms.
-  """
-  if n <= 1:
-    # Complete here
-  else:
-    a, b = # complete here
-    for _ in range(2, n + 1):
-      c = a + b
-      # Complete here
-    return # add the variable to be returned
+    Returns:
+        list: A list containing the first n terms of the Fibonacci sequence.
+    """
+    fibonacci_sequence = [0, 1]
+    if n <= 2:
+        return fibonacci_sequence[:n]
+    else:
+        for i in range(2, n):
+            next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_term)
+        return fibonacci_sequence
 
-# Get the number of terms from the user
-num_terms = int(input("Enter the number of terms: "))
+def main():
+    n = int(input("Enter a number  "))
+    fibonacci_sequence = generate_fibonacci(n)
+    print("Fibonacci sequence for :")
+    print(fibonacci_sequence)
 
-# Generate the Fibonacci sequence
-fibonacci_sequence = []
-for i in range(num_terms):
-  fibonacci_sequence.append(fibonacci(i))
-
-# Print the Fibonacci sequence
-print(fibonacci_sequence)
-
-
-# Your program should:
-
-# Ask the user to input the value of n.
-# Create a function that takes n as a parameter and returns a list containing the first n terms of the Fibonacci sequence.
-# Print the generated Fibonacci sequence.
-
+if __name__ == "__main__":
+    main()
