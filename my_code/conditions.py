82b7cbe0c3ed1ed7ebb214f96b365857ee1dad9e
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-17 17:39:32 +0300

    program to check users eligibility to vote

---
+++ my_code/conditions.py
@@ -0,0 +1,15 @@
+#
+#* Create a Python program that:
+
+#~ - Prompts a user to enter their age.
+#~ - Uses a conditional statement to check if the age is greater than or equal to 18.
+#~ - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+# Prompt the user to enter their age
+age = int(input("Enter your age: "))
+
+# Check if the age is greater than or equal to 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
