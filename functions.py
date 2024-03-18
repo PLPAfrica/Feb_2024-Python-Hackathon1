f59538eabd98ddd2e156aed7b9876cb655b1e37f
Kokoda Yasini <yasinimudy@gmail.com>
2024-03-18 21:21:49 +0100

    Creat fibbonacci sequence and voting classifier.

--- functions.py
+++ functions.py
@@ -1,7 +1,3 @@
-# Functions & Fibonacci Sequence
-# Question
-# Write a Python program to generate the Fibonacci sequence up to a specified term n. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms.
-#We have provided  you with in-complete code, from the Knowledge learned from week 1 to week 3 please complete the missing parts to achieve the goal of the question.
 def fibonacci(n):
   """
   This function generates the Fibonacci sequence up to a specified term n using iteration.
@@ -13,13 +9,13 @@ def fibonacci(n):
       A list containing the Fibonacci sequence up to n terms.
   """
   if n <= 1:
-    # Complete here
+    return n
   else:
-    a, b = # complete here
+    a, b = 0, 1
     for _ in range(2, n + 1):
       c = a + b
-      # Complete here
-    return # add the variable to be returned
+      a, b = b, c
+    return c
 
 # Get the number of terms from the user
 num_terms = int(input("Enter the number of terms: "))
@@ -31,11 +27,3 @@ for i in range(num_terms):
 
 # Print the Fibonacci sequence
 print(fibonacci_sequence)
-
-
-# Your program should:
-
-# Ask the user to input the value of n.
-# Create a function that takes n as a parameter and returns a list containing the first n terms of the Fibonacci sequence.
-# Print the generated Fibonacci sequence.
-
