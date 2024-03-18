48d265f4b017771ce6bbb0bbbd03ecc61c11529b
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-03-18 22:17:54 +0300

    Finished fib sequence

--- functions.py
+++ functions.py
@@ -14,22 +14,24 @@ def fibonacci(n):
   """
   if n <= 1:
     return [0] if n == 1 else [0, 1]
+  # above condition returms [0] if n is 1 and [0, 1] if n is 2
+
   else:
-    a, b = 0, 1
+    fibonacci_sequence =[0, 1] # initialize list with the first two terms
     for _ in range(2, n + 1):
-      c = a + b
-      a, b = b, c
-      # Complete here
-      return c # add the variable to be returned
+      
+      c = fibonacci_sequence[-1] + fibonacci_sequence[-2] #calculate next term
+      
+      fibonacci_sequence.append(c) # add the next term to the sequence
+      #return the generated fib sequence for every iteration
+    return fibonacci_sequence
   
 
 # Get the number of terms from the user
 num_terms = int(input("Enter the number of terms: "))
 
 # Generate the Fibonacci sequence
-fibonacci_sequence = []
-for i in range(num_terms):
-  fibonacci_sequence.append(fibonacci(i))
+fibonacci_sequence = fibonacci(num_terms)
 
 # Print the Fibonacci sequence
 print(fibonacci_sequence)
