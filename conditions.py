242ce385dcb5d7005d32c23058b241985cbdadba
KaKaCodes1 <hamisig123@gmail.com>
2024-03-18 23:26:23 +0300

    My Hackathon submission

--- conditions.py
+++ conditions.py
@@ -9,3 +9,9 @@
 # - Prompts a user to enter their age.
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+age=int(input('Enter your age:\n'))
+if age>=18:
+    print(f"You are {age} and you are elegible to vote")
+else:
+    print(f"You are {age} and you are not elegible to vote")
--- functions.py
+++ functions.py
@@ -13,13 +13,13 @@ def fibonacci(n):
       A list containing the Fibonacci sequence up to n terms.
   """
   if n <= 1:
-    # Complete here
+    return n
   else:
-    a, b = # complete here
+    a, b = 0,1
     for _ in range(2, n + 1):
       c = a + b
-      # Complete here
-    return # add the variable to be returned
+      a,b=b,c
+    return c
 
 # Get the number of terms from the user
 num_terms = int(input("Enter the number of terms: "))
