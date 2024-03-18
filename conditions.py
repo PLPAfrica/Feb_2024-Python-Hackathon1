e05f11ba92c894056f8f5e972b62164b0a28627f
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-03-18 19:10:57 +0300

    Steven Simoni workout on Python First Hackathon

--- conditions.py
+++ conditions.py
@@ -1,11 +1,10 @@
-# Python Conditional Statements 
-#example is https://plpacademy.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
+def user_age():
+    ''' This program asks for user age and returna message if the user is eligible for voting or not '''
+    age = int(input('Enter Your Age'))
+    if age >= 18:
+        print("You are eligible to vote")
+    else:
+        print("You are not eligible to vote")
 
 
-
-# Create a Python program that:
-
-
-# - Prompts a user to enter their age.
-# - Uses a conditional statement to check if the age is greater than or equal to 18.
-# - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+user_age()
