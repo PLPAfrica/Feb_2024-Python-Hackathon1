63c8c872aada24c02e8773b9b215b8594d586187
Nicholas Odhiambo <klaus@localhost.localdomain>
2024-03-18 18:29:32 +0300

    modified the function and conitions file

--- conditions.py
+++ conditions.py
@@ -5,7 +5,17 @@
 
 # Create a Python program that:
 
-
 # - Prompts a user to enter their age.
+try:
+    age = int(input('Enter your age: '))
+except:
+    print('Please enter a valid age.')
+
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+if age > 18:
+    print('You are eligible to vote.')
+else:
+    print('You are not eligible to vote.')
+
