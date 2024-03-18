89381592d91ea746fb9e4bb9ed2845d37864724a
Sharon404 <aokosharo@gmail.com>
2024-03-18 17:05:53 +0300

    Hackathon

--- conditions.py
+++ conditions.py
@@ -9,3 +9,14 @@
 # - Prompts a user to enter their age.
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+age = int(input('Enter your age: '))
+
+while True:
+    if 150 >= age >= 18:
+        print(f'{age} is an eligible age to vote')
+    elif 18 > age >=0:
+        print(f'{age} is too young to vote, not eligible')
+    else:
+        print('Enter a valid age!')
+    break
--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@
 # Write a Python program to generate the Fibonacci sequence up to a specified term n. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms.
 #We have provided  you with in-complete code, from the Knowledge learned from week 1 to week 3 please complete the missing parts to achieve the goal of the question.
 def fibonacci(n):
+  fibonacci_sequence = [0, 1]
   """
   This function generates the Fibonacci sequence up to a specified term n using iteration.
 
@@ -12,14 +13,17 @@ def fibonacci(n):
   Returns:
       A list containing the Fibonacci sequence up to n terms.
   """
-  if n <= 1:
+  if n < 0:
     # Complete here
+    print('Invalid number')
   else:
-    a, b = # complete here
+    a, b = 0, 1 # complete here
     for _ in range(2, n + 1):
       c = a + b
+      a, b = b, c
+      fibonacci_sequence.append(c)
       # Complete here
-    return # add the variable to be returned
+    return fibonacci_sequence # add the variable to be returned
 
 # Get the number of terms from the user
 num_terms = int(input("Enter the number of terms: "))
