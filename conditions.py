70e4d7892931c08434087d0d313f9807cb28cbdd
Josphat Kituku <kitukujosphat@gmail.com>
2024-03-18 22:34:51 +0300

    Conditions statements and Functions in python

--- conditions.py
+++ conditions.py
@@ -1,4 +1,4 @@
-# Python Conditional Statements 
+# Python Conditional Statements
 #example is https://plpacademy.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
 
 
@@ -9,3 +9,16 @@
 # - Prompts a user to enter their age.
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+def main():
+    # Prompting the user to enter their age
+    age = int(input("Please enter your age: "))
+
+    # Checking if the age is greater than or equal to 18
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+
+if __name__ == "__main__":
+    main()
