c0ab028f189820619573820f30e84f15e5bb4a00
lorineabdul@gmail.com <lorineabdul@gmail.com>
2024-03-18 20:53:43 +0300

    Wrote a Python program to generate the Fibonacci sequence up to a specified term 'n'and created a python program that prompts a user to enter their age

---
+++ conditional_statements.py
@@ -0,0 +1,14 @@
+def main():
+    # Prompt the user to enter their age
+
+    age = int(input("Please enter your age: "))
+
+    # Check if the age is greater than or equal to 18
+
+    if age >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+
+if __name__ == "__main__":
+    main(
