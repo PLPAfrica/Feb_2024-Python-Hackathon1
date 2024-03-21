80054493b05f20a1919fb1cb8460e2d42e4950f5
David Nanjila <nanjiladavid2@gmail.com>
2024-03-21 06:42:40 +0300

    Chore: Create a function to check whether a user is eligible to vote or not

--- conditions.py
+++ conditions.py
@@ -1,21 +1,16 @@
-# Python Conditional Statements 
-#example is https://plpacademy.powerlearnproject.org/course-module/62fbec9d28ac4762bc524f92/week/62fe1efd28ac4762bc524f9c/lesson/62fe1fbd28ac4762bc524f9f
 
-
-
-# Create a Python program that:
-
-
-# - Prompts a user to enter their age.
-# - Uses a conditional statement to check if the age is greater than or equal to 18.
-# - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
-
-
-
-age = int(input("Enter your age: "))
-
-
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
+while True:
+    age = int(input("Enter your age or 0 to exit: "))
+    name = input("Enter your name: ")
+    if age == 0:
+        break
+    elif age >= 18:
+        print(f"\nDear {name} You are eligible to vote.\nPlease vote wisely.\n")
+    else:
+        print("\nSorry {} You are not eligible to vote.".format(name))
+        print("You can vote after {} years.".format(18 - age))
+        quit = input("Do you want to continue? (y/n): ")
+        if quit == 'y' or quit == 'Y':
+            break
+        else:
+            continue
--- functions.py
+++ functions.py
@@ -8,25 +8,4 @@ def fibonacci(n):
     Returns:
         A list containing the Fibonacci sequence up to n terms.
     """
-    fibonacci_sequence = [] 
-    if n <= 0:
-        return fibonacci_sequence  
-    elif n == 1:
-        fibonacci_sequence.append(0)  
-    else:
-        fibonacci_sequence.extend([0, 1])  # If n is greater than 1, add the first two terms (0 and 1) to the sequence
-        a, b = 0, 1  
-        for _ in range(2, n):
-            c = a + b  
-            fibonacci_sequence.append(c)  
-            a, b = b, c  
-    return fibonacci_sequence
-
-# Get the number of terms from the user
-num_terms = int(input("Enter the number of terms: "))
-
-# Generate the Fibonacci sequence
-fibonacci_sequence = fibonacci(num_terms)
-
-# Print the Fibonacci sequence
-print(fibonacci_sequence)
+    
