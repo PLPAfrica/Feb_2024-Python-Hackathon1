4f2de8126cbfad179e1c2c00766c3fe0455f2939
David Nanjila <nanjiladavid2@gmail.com>
2024-03-21 06:53:14 +0300

    Chore: create a function to generate Finnabocci sequence upto specified term n

--- functions.py
+++ functions.py
@@ -1,11 +1,8 @@
-def fibonacci(n):
-    """
-    This function generates the Fibonacci sequence up to a specified term n using iteration.
-
-    Args:
-        n: The number of terms in the Fibonacci sequence.
-
-    Returns:
-        A list containing the Fibonacci sequence up to n terms.
-    """
-    
+#Python program to generate the Fibonacci sequence up to a specified term n. The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding terms.
+def fibonacci_generator(n):
+    a, b = 0, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+n = int(input("Enter the number of terms: "))
+print(f"\n{list(fibonacci_generator(n))}+\n")
